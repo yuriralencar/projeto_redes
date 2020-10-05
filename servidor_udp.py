fc4511941b30d7f344ab9d7539593ca34a2a780f
Gustavo Nascimento <gustavoprazeres.nascimento@gmail.com>
2020-10-04 21:51:04 -0300

    Update servidor_udp.py

--- servidor_udp.py
+++ servidor_udp.py
@@ -1,12 +1,28 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 
-server_socket = socket(AF_INET, SOCK_DGRAM)
+#Criacao do socket
+UDPServerSocket = socket(AF_INET, SOCK_DGRAM)
+UDPServerSocket.bind(('192.168.1.65', 9500))
 
-server_socket.bind(('172.23.30.241', 9500))
 
-# server_socket.listen()
+print("Servidor UDP escutando requisicoes")
 
-while True:
-    data, client_adress = server_socket.recvfrom(2048)
 
-    print(f'O cliente {client_adress} mandou >>> {data.decode()}')
+while(True):
+
+    #Recebe mensagem de cliente
+    mensagem_cliente = UDPServerSocket.recvfrom(1024)
+
+    #mensagem_cliente = (mensagem em bytes,(ip_cliente,porta))
+    
+    data = mensagem_cliente[0].decode()
+    endereco_cliente = mensagem_cliente[1]
+    ip_cliente = mensagem_cliente[1][0]
+
+    
+    print(" ")
+    print((f'O cliente ({ip_cliente}) enviou: {data}'))
+
+    #Responde para cliente
+    resposta_cliente = str.encode("Mensagem recebida com sucesso")
+    UDPServerSocket.sendto(resposta_cliente, endereco_cliente)
