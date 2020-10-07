c96e4d70966bb1ac5f14969a4c45651865f595a0
yuriMitte <yuri.alencar@mittetecnologia.com.br>
2020-10-07 18:33:04 -0300

    Create servidor_udp.py

---
+++ Projeto 1/servidor_udp.py
@@ -0,0 +1,28 @@
+from socket import socket, AF_INET, SOCK_DGRAM
+
+#Criacao do socket
+UDPServerSocket = socket(AF_INET, SOCK_DGRAM)
+UDPServerSocket.bind(('192.168.1.65', 9500))
+
+
+print("Servidor UDP escutando requisicoes")
+
+
+while(True):
+
+    #Recebe mensagem de cliente
+    mensagem_cliente = UDPServerSocket.recvfrom(1024)
+
+    #mensagem_cliente = (mensagem em bytes,(ip_cliente,porta))
+    
+    data = mensagem_cliente[0].decode()
+    endereco_cliente = mensagem_cliente[1]
+    ip_cliente = mensagem_cliente[1][0]
+
+    
+    print(" ")
+    print((f'O cliente ({ip_cliente}) enviou: {data}'))
+
+    #Responde para cliente
+    resposta_cliente = str.encode("Mensagem recebida com sucesso")
+    UDPServerSocket.sendto(resposta_cliente, endereco_cliente)
