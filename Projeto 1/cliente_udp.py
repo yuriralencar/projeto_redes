4a9557d4f0fdc5938631ba29bc61323e1e6922d6
yuriMitte <yuri.alencar@mittetecnologia.com.br>
2020-10-07 19:50:17 -0300

    Update cliente_udp.py

--- Projeto 1/cliente_udp.py
+++ Projeto 1/cliente_udp.py
@@ -2,20 +2,23 @@ from socket import socket, AF_INET, SOCK_DGRAM
 
 UDPClientSocket = socket(AF_INET, SOCK_DGRAM)
 
-while(True):
+print("---------------------------------")
+print("    BEM VINDO AO JOGUINHO        ")
+print("Por, favor selecione a opção desejada: ")
+print(" 1 - Iniciar Jogo ")
+print(" 2 - Visualizar os maiores rankings ")
+print("---------------------------------")
+#Criação da mensagem a ser enviada
+mensagem_cliente = (input("Enviar para o servidor: ")).encode()
 
-    print(" ")
-    #Criação da mensagem a ser enviada
-    mensagem_cliente = (input("Enviar para o servidor: ")).encode()
 
+#Envia a mensagem para o servidor
+UDPClientSocket.sendto(mensagem_cliente, ('172.23.0.8', 9500))
 
-    #Envia a mensagem para o servidor
-    UDPClientSocket.sendto(mensagem_cliente, ('172.23.0.6', 9500))
+#Recebe resposta do servidor   ([0] = mensagem, [1] = endereco ([0]IP, [1]PORTA))
+resposta_servidor = UDPClientSocket.recvfrom(1024)
 
-    #Recebe resposta do servidor   ([0] = mensagem, [1] = endereco ([0]IP, [1]PORTA))
-    resposta_servidor = UDPClientSocket.recvfrom(1024)
+msgServidor = resposta_servidor[0].decode()
+ipServidor = resposta_servidor[1][0]
 
-    msgServidor = resposta_servidor[0].decode()
-    ipServidor = resposta_servidor[1][0]
-
-    print((f'O servidor {ipServidor} respondeu: {msgServidor}'))
+print((f'{msgServidor}'))
