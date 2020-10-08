270ffe35f1855f738e4e31b3a9e58db251a7ea95
yuriMitte <yuri.alencar@mittetecnologia.com.br>
2020-10-07 21:18:11 -0300

    Update servidor_udp.py

--- Projeto 1/servidor_udp.py
+++ Projeto 1/servidor_udp.py
@@ -33,16 +33,45 @@ while len(listaJogadores) < 1:
         UDPServerSocket.sendto(resposta_cliente, endereco_cliente)
 
 #trabalhar com o arquivo de texto
-pergunta = ("Tu é gay?", "GRAÇAS A DEUS")
+
+for k in range(0,5):
+    
+    print(k)
+
+    pergunta = ("Tu é gay?", "NAO")
+
+    for c in listaJogadores:
+        resposta_cliente = str.encode(pergunta[0])
+        UDPServerSocket.sendto(resposta_cliente, c)
+
+    cond = True
+    while cond:
+        mensagem_cliente = UDPServerSocket.recvfrom(1024)
+        data = mensagem_cliente[0].decode()
+        endereco_cliente = mensagem_cliente[1]
+
+        if data != pergunta[1]:
+            resposta_cliente = str.encode("400")
+            UDPServerSocket.sendto(resposta_cliente, endereco_cliente)
+            posicao = listaJogadores.index(endereco_cliente)
+            Pontuacao[posicao] -= 5
+        elif data == pergunta[1]:
+            if k == 4:
+                print("aqui")
+                resposta_cliente = str.encode("600")
+            else:
+                resposta_cliente = str.encode("500")
+            UDPServerSocket.sendto(resposta_cliente, endereco_cliente)
+            acertou = endereco_cliente
+            posicao = listaJogadores.index(endereco_cliente)
+            Pontuacao[posicao] += 25
+            break
+
+
 for c in listaJogadores:
-    resposta_cliente = str.encode(pergunta[0])
+    resposta_cliente = str.encode("False")
     UDPServerSocket.sendto(resposta_cliente, c)
 
-mensagem_cliente = UDPServerSocket.recvfrom(1024)
-
-data = mensagem_cliente[0].decode()
-endereco_cliente = mensagem_cliente[1]
-
-print(data)
+print(Pontuacao)
 
 
