597a2c6f5722abad0a8b48fae01c258d17c821c5
viniciussantb <vsb@cin.ufpe.br>
2020-10-13 16:54:49 -0300

    lê arquivo
    Função pra ler o arquivo arquivo de texto, precisa mudar o path do arquivo pra cada pc.

--- Projeto 1/cliente_udp.py
+++ Projeto 1/cliente_udp.py
@@ -8,14 +8,13 @@ print("Por, favor selecione a opção desejada: ")
 print(" 1 - Iniciar Jogo ")
 print(" 2 - Visualizar os maiores rankings ")
 print("---------------------------------")
-#Criação da mensagem a ser enviada
+# Criação da mensagem a ser enviada
 mensagem_cliente = (input("Opção desejada: ")).encode()
 
-
-#Envia a mensagem para o servidor
+# Envia a mensagem para o servidor
 UDPClientSocket.sendto(mensagem_cliente, ('localhost', 9500))
 
-#Recebe resposta do servidor   ([0] = mensagem, [1] = endereco ([0]IP, [1]PORTA))
+# Recebe resposta do servidor   ([0] = mensagem, [1] = endereco ([0]IP, [1]PORTA))
 resposta_servidor = UDPClientSocket.recvfrom(1024)
 
 msgServidor = resposta_servidor[0].decode()
@@ -27,16 +26,17 @@ for c in range(5):
     resposta_servidor = UDPClientSocket.recvfrom(1024)
     msgServidor = resposta_servidor[0].decode()
 
+    print(f'Pergunta {c+1}: ')
     print(f'{msgServidor}')
-    
+
     mensagem_cliente = (input("Digite sua resposta: ")).encode()
     UDPClientSocket.sendto(mensagem_cliente, ('localhost', 9500))
 
     resposta_servidor = UDPClientSocket.recvfrom(1024)
     msgServidor = resposta_servidor[0].decode()
-    
+
     while True:
-        
+
         if msgServidor != "False":
             if msgServidor == "400":
                 mensagem_cliente = (input("Wrong answer.. tente novamente: ")).encode()
@@ -48,9 +48,12 @@ for c in range(5):
                 if msgServidor == "500":
                     print("Resposta correta. Próxima pergunta --> ")
                 else:
-                    print("Fim de jogo! Mas tu é gay, para de negar!")   
+                    print("Fim de jogo! Mas tu é gay, para de negar!")
                 break
         else:
             break
 
+resposta_servidor = UDPClientSocket.recvfrom(1024)
+msgServidor = resposta_servidor[0].decode()
+print(msgServidor)
 
--- Projeto 1/servidor_udp.py
+++ Projeto 1/servidor_udp.py
@@ -1,30 +1,67 @@
-import time
 from socket import socket, AF_INET, SOCK_DGRAM
 
-#Criacao do socket
+
+def num(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def elimina_n(palavra):
+    '''Elimina o \n'''
+    novaString = ""
+    for caracter in palavra:
+        if caracter != "\n":
+            novaString += caracter
+    return novaString
+
+
+def le_arquivo(arquivo):
+    '''Cria nova lista sem \n'''
+    novaLista = []
+    cont = 0
+    tupla = ()
+    for palavra in arquivo:
+        cont += 1
+        caracter = elimina_n(palavra)
+        print(caracter)
+        tupla1 = (caracter,)
+        tupla += tupla1
+        if num(cont):
+            novaLista.append(tupla)
+            tupla = ()
+    return novaLista
+
+
+# Criacao do socket
 UDPServerSocket = socket(AF_INET, SOCK_DGRAM)
 UDPServerSocket.bind(('localhost', 9500))
 
-#print("Servidor UDP escutando requisicoes")
+# print("Servidor UDP escutando requisicoes")
+
+print("procurando por conexões...")
+
 
 listaJogadores = []
 Pontuacao = []
-while len(listaJogadores) < 1:
+while True:
 
-    #Recebe mensagem de cliente
+    # Recebe mensagem de cliente
     mensagem_cliente = UDPServerSocket.recvfrom(1024)
 
     data = mensagem_cliente[0].decode()
     endereco_cliente = mensagem_cliente[1]
     ip_cliente = mensagem_cliente[1][0]
-    
+    print(f'Jogador {endereco_cliente} conectado!')
+
     listaJogadores.append(endereco_cliente)
     Pontuacao.append(0)
 
-    
     if data == "1":
         resposta_cliente = str.encode("Estamos procurando oponentes para você...")
         UDPServerSocket.sendto(resposta_cliente, endereco_cliente)
+        break
     elif data == "2":
         resposta_cliente = str.encode("Aqui está a lista com as maiores pontuações")
         UDPServerSocket.sendto(resposta_cliente, endereco_cliente)
@@ -32,16 +69,18 @@ while len(listaJogadores) < 1:
         resposta_cliente = str.encode("Entrada inválida")
         UDPServerSocket.sendto(resposta_cliente, endereco_cliente)
 
-#trabalhar com o arquivo de texto
+# trabalhar com o arquivo de texto
 
-for k in range(0,5):
-    
+for k in range(0, 5):
+
     print(k)
-
-    pergunta = ("Tu é gay?", "NAO")
+    arquivo = open("C:\\Users\\vinny\\Desktop\\projeto_redes.txt", "r")
+    perguntas = le_arquivo(arquivo)
+    print(perguntas)
+    arquivo.close()
 
     for c in listaJogadores:
-        resposta_cliente = str.encode(pergunta[0])
+        resposta_cliente = str.encode(perguntas[k][0])
         UDPServerSocket.sendto(resposta_cliente, c)
 
     cond = True
@@ -50,12 +89,12 @@ for k in range(0,5):
         data = mensagem_cliente[0].decode()
         endereco_cliente = mensagem_cliente[1]
 
-        if data != pergunta[1]:
+        if data != perguntas[k][1]:
             resposta_cliente = str.encode("400")
             UDPServerSocket.sendto(resposta_cliente, endereco_cliente)
             posicao = listaJogadores.index(endereco_cliente)
             Pontuacao[posicao] -= 5
-        elif data == pergunta[1]:
+        elif data == perguntas[k][1]:
             if k == 4:
                 print("aqui")
                 resposta_cliente = str.encode("600")
@@ -65,13 +104,18 @@ for k in range(0,5):
             acertou = endereco_cliente
             posicao = listaJogadores.index(endereco_cliente)
             Pontuacao[posicao] += 25
-            break
+            cond = False
 
 
-for c in listaJogadores:
-    resposta_cliente = str.encode("False")
-    UDPServerSocket.sendto(resposta_cliente, c)
+mensagem_cliente = ''
+cont = 0
+for pos in Pontuacao:
+    cont += 1
+    mensagem_cliente += str(cont) + ' -> ' + str(pos)
+resposta_cliente = str.encode(mensagem_cliente)
+for cliente in listaJogadores:
+    print(mensagem_cliente)
+    UDPServerSocket.sendto(resposta_cliente, cliente)
 
-print(Pontuacao)
 
 
